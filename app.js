f5c44de046868c06d3762f1c1b672630e01bc761
Christian Dupuis <cd@atomist.com>
2019-11-10 02:08:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
-
+  
 /**
  * API keys and Passport configuration.
  */
